8a058e98f5e89de72cdb23cfae61b79cd01040b6
Roland Fazakas <fazakasr@coventry.ac.uk>
2019-02-12 01:13:49 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -178,7 +178,7 @@ describe "#display_board in 'lib/display_board.rb" do
     it 'prints an entire board full of Os' do
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
-      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O"]
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
