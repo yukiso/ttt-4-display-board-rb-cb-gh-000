d2224ad5fb8b2652206ce0d0f402e93a04ca34de
Roland Fazakas <fazakasr@coventry.ac.uk>
2019-02-12 01:14:33 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -179,6 +179,8 @@ describe "#display_board in 'lib/display_board.rb" do
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
       board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+      output = capture_puts{ display_board(board) } if defined?(display_board)
+      rows = output.split("\n")
       # Hint: You should be able to copy the code in the previous it example
       # and make a few simple edits to convert the previous example to this
       # example's situation.
